0530fcbd620a7553d484787b4aa10c141c808ed6
puising <78009203+puising@users.noreply.github.com>
2021-02-02 06:21:49 -0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,17 @@
+
+import sys
+
+print(sys.version)
+print(sys.executable)
+
+
+name=input("whats your name?")
+print("hello"+name)
+print(max("hello world"))
+print(min("hello world"))
+print(len("hello world"))
+quotient=7//3
+print(quotient)
+remainder=7%3
+print(remainder)
+print(len("my name is jane"))
